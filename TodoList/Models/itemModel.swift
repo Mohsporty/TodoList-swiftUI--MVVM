1bdfcd410fe0b0b54c5706608d2f25f1688e29f8
Mohsporty <m.alassdi@gmail.com>
2024-11-05 03:03:36 +0300

    new update editing font and desigh into the 2 page

---
+++ TodoList/Models/itemModel.swift
@@ -0,0 +1,8 @@
+//
+//  itemModel.swift
+//  TodoList
+//
+//  Created by Mohammad  on 05/11/2024.
+//
+
+import Foundation
---
+++ TodoList/Views/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  TodoList
+//
+//  Created by Mohammad  on 04/11/2024.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
