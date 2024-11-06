317518c384dc2c86b708fcf66278e359d7a8dc08
Mohsporty <m.alassdi@gmail.com>
2024-11-06 23:58:49 +0300

    Add new button if the page is empty

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Mohammad  on 06/11/2024.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
